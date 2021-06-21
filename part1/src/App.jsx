fb677eb7be048c2ddcb16e6f237b5d845c91c077
ArunKumar Nadikattu <47110396+mastrero@users.noreply.github.com>
2021-06-21 11:43:36 +0000

    Exercise 1.2: course information, step2

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -30,17 +30,17 @@ export default App;
 const Header = ({ course }) => <h1>{course}</h1>;
 
 const Content = ({ part1, exercises1, part2, exercises2, part3, exercises3 }) => (
-  <>
-    <p>
-      {part1} {exercises1}
-    </p>
-    <p>
-      {part2} {exercises2}
-    </p>
-    <p>
-      {part3} {exercises3}
-    </p>
-  </>
+  <div>
+    <Part part={part1} exercises={exercises1} />
+    <Part part={part2} exercises={exercises2} />
+    <Part part={part3} exercises={exercises3} />
+  </div>
+);
+
+const Part = ({ part, exercises }) => (
+  <p>
+    {part} {exercises}
+  </p>
 );
 
 const Total = ({ exercises1, exercises2, exercises3 }) => (
